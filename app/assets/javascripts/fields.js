a36860f5b7054e2903c3508acefd0bc49a2a7f96
Thomas Cannon <tcannon@codemonkeydev.com>
2014-04-16 00:36:04 -0400

    Improve Gallery Creation/Editing Form
    * Looks nicer, correctly removes entries

--- app/assets/javascripts/fields.js
+++ app/assets/javascripts/fields.js
@@ -1,11 +1,11 @@
 function remove_fields(link) {
-  $(link).prev("input[type=hidden]").value = "1";
-  $(link).closest(".fields").remove();
+  $(link).prev("input[type=hidden]")[0].value = "1";
+  $(link).closest(".fields").hide();
 }
 
-function add_fields(link, association, content) {
+function add_fields(parent_element_query, association, content) {
   var new_id = new Date().getTime();
   var regexp = new RegExp("new_" + association, "g")
   var container = document.getElementById("image_container");
-  $(link).before(content.replace(regexp, new_id));
+  $(parent_element_query).append(content.replace(regexp, new_id));
 }
