353309c877e2a7b2d1a3cbc0b28c015723c67437
Thomas Cannon <tcannon@codemonkeydev.com>
2014-04-16 00:27:09 -0400

    Images must require URL and description, gallery validates images

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,6 +2,8 @@ class Gallery < ActiveRecord::Base
   has_many :images
   attr_accessible :images_attributes, :description, :title
   accepts_nested_attributes_for :images, allow_destroy: true
+  validates_associated :images
+  validates :images, :presence => true
 
   validates :title, :presence => true, :uniqueness => true, :allow_blank => false
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,6 @@
 class Image < ActiveRecord::Base
   belongs_to :gallery
   attr_accessible :gallery_id, :description, :url
+  validates :url, :presence => true, :allow_blank => false
+  validates :description, :presence => true, :allow_blank => false
 end
